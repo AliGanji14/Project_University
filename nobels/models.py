bf99837fcd563a0012e18fb0573bc672bdd79a94
Ali Ganji <aliganji142879@gmail.com>
2023-06-11 23:32:31 +0330

    add Comment nobel

--- nobels/models.py
+++ nobels/models.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Nobel(models.Model):
@@ -16,7 +17,7 @@ class Nobel(models.Model):
     year = models.PositiveIntegerField()
     country = models.CharField(max_length=50)
     grouping = models.CharField(choices=STATUS_CHOICES, max_length=23)
-    cover = models.ImageField(upload_to="covers/",blank=True)
+    cover = models.ImageField(upload_to="covers/", blank=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
@@ -25,3 +26,13 @@ class Nobel(models.Model):
 
     def get_absolute_url(self):
         return reverse("nobel_detail", args=[self.id])
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    nobel = models.ForeignKey(Nobel, on_delete=models.CASCADE)
+    text = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
